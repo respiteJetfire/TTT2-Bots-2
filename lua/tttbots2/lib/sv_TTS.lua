7c445d2dec74c267591b3704f61bff8132059d98
Callum Yohn <armedjetfire23@hotmail.co.uk>
2024-12-09 17:06:55 +0000

    Refactor Azure TTS integration: streamline voice selection, enhance error handling, and improve SSML request processing

--- lua/tttbots2/lib/sv_TTS.lua
+++ lua/tttbots2/lib/sv_TTS.lua
@@ -160,117 +160,113 @@ function TTTBots.TTS.ElevenLabsSendRequest(ply, text, teamOnly)
     })
 end
 
+local function handleTTSResponse(code, body, chatter, teamOnly, ply, text, ssmlBody)
+    if code == 200 then
+        local FileContent = util.Compress(body)
+        local FileSize = #FileContent
+        local FileID = os.time()
+        local FileMaxSize = 63000
+
+        if FileSize > FileMaxSize then
+            local FileParts = math.ceil(FileSize / FileMaxSize)
+            local FileTable = {}
+
+            for i = 1, FileParts - 1 do
+                local IndexStart = (i - 1) * FileMaxSize + 1
+                local IndexEnd = i * FileMaxSize
+                local FileData = string.sub(FileContent, IndexStart, IndexEnd)
+                FileTable[i] = FileData
+            end
+
+            local IndexStart = (FileParts - 1) * FileMaxSize + 1
+            local FileData = string.sub(FileContent, IndexStart)
+            FileTable[FileParts] = FileData
+
+            TTTBots.TTS.Cache[FileID] = FileTable
+            TTTBots.TTS.Cache[FileID .. "_pos"] = 0
+
+            timer.Create("azure_send_" .. FileID, 1 / 20, #TTTBots.TTS.Cache[FileID], function()
+                local FilePos = TTTBots.TTS.Cache[FileID .. "_pos"] + 1
+                TTTBots.TTS.Cache[FileID .. "_pos"] = FilePos
+                chatter:WriteDataFree(teamOnly, ply, false, FileID, TTTBots.TTS.Cache[FileID][FilePos], FilePos, FileParts)
+            end)
+        else
+            chatter:WriteDataFree(teamOnly, ply, true, FileID, FileContent)
+        end
+    else
+        print("The HTTP request to Azure TTS API failed. HTTP Code: " .. code .. ". Response body: " .. body)
+        local response = util.JSONToTable(body)
+        if response and response.error and response.error.message then
+            print("Azure API Error: " .. response.error.message)
+        end
+        print("SSML Body: " .. ssmlBody) -- Print the SSML body for debugging
+        chatter:Say(text, teamOnly)
+    end
+end
+
+local function handleTokenResponse(body, len, headers, code, azureTTSEndpoint, azureResourceGroupName, azureVoiceName, text, chatter, teamOnly, ply)
+    if code == 200 then
+        local azureToken = body
+        local azureVoiceQuality = {
+            "riff-8khz-8bit-mono-alaw",
+            "riff-22050hz-16bit-mono-pcm",
+            "riff-24khz-16bit-mono-pcm",
+            "riff-44100hz-16bit-mono-pcm",
+            "riff-48khz-16bit-mono-pcm"
+        }
+
+        local qualityIndex = TTTBots.Lib.GetConVarInt("chatter_voice_azure_voice_quality")
+        if qualityIndex < 1 or qualityIndex > #azureVoiceQuality then
+            qualityIndex = 2 -- Default to "audio-16khz-16kbitrate-mono-mp3"
+        end
+
+        local outputFormat = azureVoiceQuality[qualityIndex]
+
+        -- Sanitize the text to remove all non-alphanumeric characters except spaces
+        text = string.gsub(text, "[^%w%s]", "")
+
+        -- Prepare the SSML request body
+        local ssmlBody = string.format(
+            "<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'><voice name='%s'>%s</voice></speak>",
+            azureVoiceName, text
+        )
+
+        -- Make an HTTP request to Azure TTS API to get the TTS audio
+        HTTP({
+            url = azureTTSEndpoint,
+            method = "POST",
+            headers = {
+                ["Content-Type"] = "application/ssml+xml",
+                ["Authorization"] = "Bearer " .. azureToken,
+                ["Connection"] = "Keep-Alive",
+                ["User-Agent"] = azureResourceGroupName,
+                ["X-Microsoft-OutputFormat"] = outputFormat,
+            },
+            body = ssmlBody,
+            success = function(code, body) handleTTSResponse(code, body, chatter, teamOnly, ply, text, ssmlBody) end,
+        })
+    else
+        print("Failed to receive Azure access token. HTTP Code: " .. code)
+        print("Response body: " .. body)
+    end
+end
+
 function TTTBots.TTS.AzureTTSSendRequest(ply, text, teamOnly)
     --- Send a request to Microsoft Azure Text-to-Speech API to get the TTS audio
     local azureRegion = TTTBots.Lib.GetConVarString("chatter_voice_azure_region")
     local azureResourceGroupName = TTTBots.Lib.GetConVarString("chatter_voice_azure_resource_name")
     local azureResourceSpeechAPIKey = TTTBots.Lib.GetConVarString("chatter_voice_azure_resource_api_key")
     local azureVoiceName = ply:BotPersonality().voice.id
-    -- print("Azure Voice Name: " .. azureVoiceName)
+    print("Azure Voice Name: " .. azureVoiceName)
     local chatter = ply:BotChatter()
     local azureTokenEndpoint = "https://" .. azureRegion .. ".api.cognitive.microsoft.com/sts/v1.0/issuetoken"
     local azureTTSEndpoint = "https://" .. azureRegion .. ".tts.speech.microsoft.com/cognitiveservices/v1"
 
-    -- Function to handle the TTS response
-    local function handleTTSResponse(code, body)
-        if code == 200 then
-            local FileContent = util.Compress(body)
-            local FileSize = #FileContent
-            local FileID = os.time()
-            local FileMaxSize = 63000
-
-            if FileSize > FileMaxSize then
-                local FileParts = math.ceil(FileSize / FileMaxSize)
-                local FileTable = {}
-
-                for i = 1, FileParts - 1 do
-                    local IndexStart = (i - 1) * FileMaxSize + 1
-                    local IndexEnd = i * FileMaxSize
-                    local FileData = string.sub(FileContent, IndexStart, IndexEnd)
-                    FileTable[i] = FileData
-                end
-
-                local IndexStart = (FileParts - 1) * FileMaxSize + 1
-                local FileData = string.sub(FileContent, IndexStart)
-                FileTable[FileParts] = FileData
-
-                TTTBots.TTS.Cache[FileID] = FileTable
-                TTTBots.TTS.Cache[FileID .. "_pos"] = 0
-
-                timer.Create("azure_send_" .. FileID, 1 / 20, #TTTBots.TTS.Cache[FileID], function()
-                    local FilePos = TTTBots.TTS.Cache[FileID .. "_pos"] + 1
-                    TTTBots.TTS.Cache[FileID .. "_pos"] = FilePos
-                    chatter:WriteDataFree(teamOnly, ply, false, FileID, TTTBots.TTS.Cache[FileID][FilePos], FilePos, FileParts)
-                end)
-            else
-                chatter:WriteDataFree(teamOnly, ply, true, FileID, FileContent)
-            end
-        else
-            print("The HTTP request to Azure TTS API failed. HTTP Code: " .. code .. ". Response body: " .. body)
-            chatter:Say(text, teamOnly)
-        end
-    end
-
-    -- Function to handle the access token response
-    local function handleTokenResponse(body, len, headers, code)
-        if code == 200 then
-            -- print("Received Azure access token.")
-            local azureToken = body
-            local azureVoiceQuality = {
-                "riff-8khz-8bit-mono-alaw",
-                "riff-22050hz-16bit-mono-pcm",
-                "riff-24khz-16bit-mono-pcm",
-                "riff-44100hz-16bit-mono-pcm",
-                "riff-48khz-16bit-mono-pcm"
-            }
-
-            local qualityIndex = TTTBots.Lib.GetConVarInt("chatter_voice_azure_voice_quality")
-            if qualityIndex < 1 or qualityIndex > #azureVoiceQuality then
-                qualityIndex = 2 -- Default to "audio-16khz-16kbitrate-mono-mp3"
-            end
-
-            local outputFormat = azureVoiceQuality[qualityIndex]
-
-            -- Prepare the SSML request body
-            local ssmlBody = string.format(
-                "<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'><voice name='%s'>%s</voice></speak>",
-                azureVoiceName, text
-            )
-
-            -- Print the SSML body for debugging
-            -- print("SSML Body:", ssmlBody)
-
-            -- Make an HTTP request to Azure TTS API to get the TTS audio
-            HTTP({
-                url = azureTTSEndpoint,
-                method = "POST",
-                headers = {
-                    ["Content-Type"] = "application/ssml+xml",
-                    ["Authorization"] = "Bearer " .. azureToken,
-                    ["Connection"] = "Keep-Alive",
-                    ["User-Agent"] = "Mozilla/5.0",
-                    ["X-Microsoft-OutputFormat"] = outputFormat,
-                },
-                body = ssmlBody,
-                success = handleTTSResponse,
-                failed = function(err)
-                    print("HTTP request to Azure TTS API failed: " .. err .. ". Response body: " .. (body or ""))
-                end
-            })
-            ---print the headers inside for debugging
-            -- for k, v in pairs(headers) do
-            --     print(k, v)
-            -- end
-        else
-            print("Failed to receive Azure access token. HTTP Code: " .. code)
-            print("Response body: " .. body)
-        end
-    end
-
     -- Get Azure access token
-    -- print("Getting Azure access token...")
     http.Post(azureTokenEndpoint, "",
-        handleTokenResponse,
+        function(body, len, headers, code)
+            handleTokenResponse(body, len, headers, code, azureTTSEndpoint, azureResourceGroupName, azureVoiceName, text, chatter, teamOnly, ply)
+        end,
         function(err)
             print("HTTP Error: " .. err)
         end,
